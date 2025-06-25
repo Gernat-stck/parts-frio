528300f25426a4623cad04caab73466f70207c6b
Gernat <nagerman26@gmail.com>
2025-06-25 13:04:19 -0600

    feat: agregar método withRole en UserFactory para asignar roles a usuarios

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,6 +5,9 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use App\Models\User;
+use App\Models\Role;
+use Illuminate\Support\Facades\DB;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -37,8 +40,25 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'email_verified_at' => null,
         ]);
+    }
+
+    /**
+     * Assign a role to the user after creating.
+     */
+    public function withRole($role)
+    {
+        return $this->afterCreating(function (User $user) use ($role) {
+            $roleModel = Role::where('slug', $role)->first();
+            if ($roleModel) {
+                // Usar role_slug en lugar de role_id
+                DB::table('role_user')->insert([
+                    'user_id' => $user->user_id,
+                    'role_slug' => $roleModel->slug
+                ]);
+            }
+        });
     }
 }
