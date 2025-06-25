4ee5ecf757043843d8945234f41884b9af1d6b2e
Gernat <nagerman26@gmail.com>
2025-06-25 14:50:38 -0600

    feat: agregar controlador AdminController para gestión de inventario y usuarios

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Service\InventoryService;
+use Inertia\Inertia;
+
+class AdminController extends Controller
+{
+    protected $inventoryService;
+
+    /**
+     * Contructor for AdminController
+     *  @param InventoryService $inventoryService
+     * 
+     */
+    public function __construct(InventoryService $inventoryService)
+    {
+        $this->inventoryService = $inventoryService;
+    }
+
+    public function index()
+    {
+        return Inertia::render('admin/dashboard');
+    }
+
+    /**
+     *  Display the inventory management page
+     *  @return \Inertia\Response
+     */
+    public function manageInventory()
+    {
+        return Inertia::render('admin/inventory/inventory', [
+            'products' => $this->inventoryService->getAllProducts(),
+        ]);
+    }
+
+    /**
+     *  Display the user management page
+     *  @return \Inertia\Response
+     */
+    public function manageUsers()
+    {
+        // Logic to manage users
+        return view('admin.users.index');
+    }
+
+}
