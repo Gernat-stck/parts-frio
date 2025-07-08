cf4579f8844b2d47374eef217bb986dd726d4bd7
Gernat <nagerman26@gmail.com>
2025-07-07 21:43:51 -0600

    feat: integrar componente WidgetSales en el panel de administración

--- resources/js/pages/admin/dashboard.tsx
+++ resources/js/pages/admin/dashboard.tsx
@@ -3,6 +3,10 @@ import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/react';
 import { adminNavItems } from '@/lib/nav-items';
+import { clientesMock } from '../../data/clientes-mock';
+import { Cliente } from '../../types/clientes';
+import { useState } from 'react';
+import WidgetSales from '../../components/sales/widget-sales';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -12,16 +16,15 @@ const breadcrumbs: BreadcrumbItem[] = [
 ];
 
 export default function Dashboard() {
+    const [clientes] = useState<Cliente[]>(clientesMock);
+
     return (
         <AppLayout breadcrumbs={breadcrumbs} mainNavItems={adminNavItems}>
             <Head title="Dashboard" />
-            <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4 overflow-x-auto">
-                <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+            <div className="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
+                <div className="grid auto-rows-min gap-4 md:grid-cols-2">
                     <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
-                    </div>
-                    <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                        <WidgetSales clientes={clientes} />
                     </div>
                     <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
                         <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
