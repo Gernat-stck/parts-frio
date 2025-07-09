372ebfdf596fbaec1468325748715c45b340a3eb
Gernat <nagerman26@gmail.com>
2025-07-09 01:18:20 -0600

    feat: add dashboard component

--- resources/js/pages/admin/dashboard.tsx
+++ resources/js/pages/admin/dashboard.tsx
@@ -1,12 +1,9 @@
-import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
+import { ScrollArea } from '@/components/ui/scroll-area';
 import AppLayout from '@/layouts/app-layout';
+import { adminNavItems } from '@/lib/nav-items';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/react';
-import { adminNavItems } from '@/lib/nav-items';
-import { clientesMock } from '../../data/clientes-mock';
-import { Cliente } from '../../types/clientes';
-import { useState } from 'react';
-import WidgetSales from '../../components/sales/widget-sales';
+import DashboardV3 from '../../components/widgets/financial-widget';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -16,24 +13,12 @@ const breadcrumbs: BreadcrumbItem[] = [
 ];
 
 export default function Dashboard() {
-    const [clientes] = useState<Cliente[]>(clientesMock);
-
     return (
         <AppLayout breadcrumbs={breadcrumbs} mainNavItems={adminNavItems}>
             <Head title="Dashboard" />
-            <div className="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
-                <div className="grid auto-rows-min gap-4 md:grid-cols-2">
-                    <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                        <WidgetSales clientes={clientes} />
-                    </div>
-                    <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
-                    </div>
-                </div>
-                <div className="relative min-h-[100vh] flex-1 overflow-hidden rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border">
-                    <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
-                </div>
-            </div>
+            <ScrollArea className="container mx-auto h-[calc(100vh-5rem)] p-5">
+                <DashboardV3 />
+            </ScrollArea>
         </AppLayout>
     );
 }
