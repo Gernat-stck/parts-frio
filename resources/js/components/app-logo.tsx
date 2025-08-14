627500f3561381c4280128b161abb38f935bbfdc
Gernat <nagerman26@gmail.com>
2025-08-14 00:18:42 -0600

    V3.0.2 -Cambio de estilos y logos -Eliminacion de rutas para Auth (No necesarios para mayor gobernanza)

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -3,11 +3,16 @@ import AppLogoIcon from './app-logo-icon';
 export default function AppLogo() {
     return (
         <>
-            <div className="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground">
-                <AppLogoIcon className="size-5 fill-current text-white dark:text-black" />
+            <div className="flex aspect-square size-10 items-center justify-center rounded-md">
+                <AppLogoIcon className="h-full w-full fill-current" />
             </div>
-            <div className="ml-1 grid flex-1 text-left text-sm">
-                <span className="mb-0.5 truncate leading-tight font-semibold">Laravel Starter Kit</span>
+            <div className="ml-1 grid flex-1 text-left">
+                <span className="mb-0.5 truncate leading-tight font-semibold text-xl">
+                    <span className="animate-pulse bg-gradient-to-r from-blue-600 via-purple-600 to-blue-800 bg-clip-text text-transparent">
+                        Nexus
+                    </span>
+                    <span className="ml-1 bg-gradient-to-r from-purple-600 via-pink-600 to-purple-800 bg-clip-text text-transparent">BitGen</span>
+                </span>
             </div>
         </>
     );
--- resources/js/layouts/auth/auth-simple-layout.tsx
+++ resources/js/layouts/auth/auth-simple-layout.tsx
@@ -1,6 +1,7 @@
 import AppLogoIcon from '@/components/app-logo-icon';
 import { Link } from '@inertiajs/react';
 import { type PropsWithChildren } from 'react';
+import { Separator } from '@/components/ui/separator';
 
 interface AuthLayoutProps {
     name?: string;
@@ -15,9 +16,10 @@ export default function AuthSimpleLayout({ children, title, description }: Props
                 <div className="flex flex-col gap-8">
                     <div className="flex flex-col items-center gap-4">
                         <Link href={route('home')} className="flex flex-col items-center gap-2 font-medium">
-                            <div className="mb-1 flex h-9 w-9 items-center justify-center rounded-md">
-                                <AppLogoIcon className="size-9 fill-current text-[var(--foreground)] dark:text-white" />
+                            <div className="mb-1 flex h-30 w-50 items-center justify-center rounded-md">
+                                <AppLogoIcon className="size-100 fill-current" />
                             </div>
+                            <Separator />
                             <span className="sr-only">{title}</span>
                         </Link>
 
